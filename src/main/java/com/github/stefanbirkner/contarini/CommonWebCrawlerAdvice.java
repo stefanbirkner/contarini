93aabea19047d04769e2e1ba9d09309a397ac836
Stefan Birkner <mail@stefan-birkner.de>
2015-02-11 09:54:32 +0100

    Use more explicit name for noodp.
    The meaning of `noodp` is rarely known. The new name
    `DONT_USE_DESCRIPTION_FROM_OPEN_DIRECTORY_PROJECT` tells
    the user more about the advice's effect.

--- src/main/java/com/github/stefanbirkner/contarini/CommonWebCrawlerAdvice.java
+++ src/main/java/com/github/stefanbirkner/contarini/CommonWebCrawlerAdvice.java
@@ -1,8 +1,17 @@
 package com.github.stefanbirkner.contarini;
 
+/**
+ * Advices that are supported by most web crawlers. See
+ * <a href="https://support.google.com/webmasters/answer/79812">Meta
+ * tags that Google understands</a> and
+ * <a href="http://www.bing.com/webmaster/help/which-robots-metatags-does-bing-support-5198d240">bing
+ * webmaster help &amp; how-to</a>
+ */
 public enum CommonWebCrawlerAdvice implements WebCrawlerAdvice {
-    NO_INDEX("noindex"), NO_FOLLOW("nofollow"), NONE("none"), NO_ARCHIVE("noarchive"), NO_SNIPPET("nosnippet"), NO_ODP(
-            "noodp"), NO_TRANSLATE("notranslate"), NO_IMAGE_INDEX("noimageindex");
+    NO_INDEX("noindex"), NO_FOLLOW("nofollow"), NONE("none"),
+    NO_ARCHIVE("noarchive"), NO_SNIPPET("nosnippet"),
+    DONT_USE_DESCRIPTION_FROM_OPEN_DIRECTORY_PROJECT("noodp"),
+    NO_TRANSLATE("notranslate"), NO_IMAGE_INDEX("noimageindex");
 
     private final String label;
 
@@ -10,6 +19,9 @@ public enum CommonWebCrawlerAdvice implements WebCrawlerAdvice {
         this.label = label;
     }
 
+    /**
+     * Returns the label that has to be used by the meta robots tag.
+     */
     @Override
     public String getLabel() {
         return label;
